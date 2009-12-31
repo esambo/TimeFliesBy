33afa5c535f1bbc04ae92df9cea14f7c1a49ef20
E. Sambo <news@emmanuelsambo.com>
2009-12-31 14:38:19 -0600

    add current gem versions with which it works right now

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -27,7 +27,7 @@ config.action_mailer.delivery_method = :test
 # like if you have constraints or database-specific column types
 # config.active_record.schema_format = :sql
 
-config.gem  'rspec-rails',  :lib  =>  false
-config.gem  'rspec',        :lib  =>  false
-config.gem  'cucumber'
-config.gem  'webrat'
+config.gem  'rspec-rails',  :lib  =>  false, :version => '>= 1.2.9'
+config.gem  'rspec',        :lib  =>  false, :version => '>= 1.2.9'
+config.gem  'cucumber',                      :version => '>= 0.4.3'
+config.gem  'webrat',                        :version => '>= 0.5.3'
