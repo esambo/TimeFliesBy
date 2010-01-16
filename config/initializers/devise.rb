d42e6940d007a6626a7437a963f0dca0e3446e93
E. Sambo <news@emmanuelsambo.com>
2010-01-16 14:21:03 -0600

    Allow newly registered users to use the application for 2 days before they have to confirm by email. No reason to lock down the application until confirmation.

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -36,7 +36,7 @@ Devise.setup do |config|
 
   # The time you want give to your user to confirm his account. During this time
   # he will be able to access your application without confirming. Default is nil.
-  # config.confirm_within = 2.days
+  config.confirm_within = 2.days
 
   # The time the user will be remembered without asking for credentials again.
   # config.remember_for = 2.weeks
