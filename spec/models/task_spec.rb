5c65f466095e367b88de933e360e52951051c3b7
E. Sambo <news@emmanuelsambo.com>
2010-02-27 11:32:52 -0600

    Refactor model test: Create task through User

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -8,35 +8,34 @@ describe Task do
       :title => "valid title",
       :start => Time.now,
       :stop => Time.now,
-      :description => "valid description",
-      :user => @valid_user
+      :description => "valid description"
     }
   end
 
   it "should only show tasks for the current user" do
     u1 = create_user(:email => 'models_test_user_3@timefliesby.com')
     Task.all.size.should == 0
-    u1.tasks.create({:title => "Task 1 for user: 1", :start => 1.minutes.ago, :stop => Time.now, :user_id => 1})
+    u1.tasks.create({:title => "Task 1 for user: 1", :start => 1.minutes.ago, :stop => Time.now})
     Task.all.size.should == 1
 
     u2 = create_user(:email => 'models_test_user_2@timefliesby.com')
     u2.tasks.all.size.should == 0
-    u2.tasks.create({:title => "Task 1 for user: 2", :start => 1.minute.ago, :stop => Time.now, :user_id => 2})
+    u2.tasks.create({:title => "Task 1 for user: 2", :start => 1.minute.ago, :stop => Time.now})
     u2.tasks.all.size.should == 1
   end
 
   it "should create a new instance given valid attributes" do
-    Task.create!(@valid_attributes)
+    @valid_user.tasks.create!(@valid_attributes)
   end
 
   it "should require start" do
-    task = Task.new
+    task = @valid_user.tasks.new
     task.should_not be_valid
     task.errors[:start].should_not be_blank
   end
 
   it "should not validate for stop before start" do
-    task = Task.new
+    task = @valid_user.tasks.new
     task.start = Time.now
     task.stop = 5.minutes.ago
     task.should_not be_valid
@@ -50,7 +49,7 @@ describe Task do
   end
 
   it "should require valid user" do
-    task = Task.new
+    task = @valid_user.tasks.new
     task.start = 1.minutes.ago
     task.stop = Time.now
     task.user_id = 9999
@@ -59,10 +58,9 @@ describe Task do
   end
 
   it "should create a valid task" do
-    task = Task.new
+    task = @valid_user.tasks.new
     task.start = 1.minutes.ago
     task.stop = Time.now
-    task.user = @valid_user
     task.should be_valid
   end
 
@@ -76,7 +74,7 @@ describe Task do
     end
 
     it "should set start and stop to Time.now when using NOW on the first task" do
-      task = Task.new
+      task = @valid_user.tasks.new
       task.now
       task.start.should == Time.now
       task.stop.should == Time.now
@@ -84,14 +82,14 @@ describe Task do
     end
 
     it "should set stop to Time.now when using NOW on new task" do
-      task = Task.new
+      task = @valid_user.tasks.new
       task.now
       task.stop.should == Time.now
     end
 
     it "should set start to previous stop when using NOW on new task" do
-      prev = Task.create! :start => 5.minutes.ago, :stop => 4.minutes.ago, :user => @valid_user
-      task = Task.new
+      prev = @valid_user.tasks.create! :start => 5.minutes.ago, :stop => 4.minutes.ago
+      task = @valid_user.tasks.new
       task.now
       task.start.should be < Time.now
       task.start.should == prev.stop
